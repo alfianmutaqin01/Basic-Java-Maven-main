8ded91a69f6bc430fcbeaee9cb18cde9c68d8e01
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-05-29 16:00:07 +0700

    Add sonar-maven-plugin to pom.xml and refactor Counter class methods in Driver

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,26 +2,38 @@ package src.main.java;
 
 public class Counter {
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    private int count;
+    
+    public Counter() {
+        this.reset();
+    }
+        
+    public void reset() {
+        count = 0;
+    }
+    
+    public void increment() {
+        count++;
+    }
+    
+    public void decrement() {
+        count--;
+    }
+    
+    public int getCount() {
+        return count;
+    }
+
+    // Tambahan metode baru
+    public void increaseBy(int value) {
+        count += value;
+    }
+
+    public void decreaseBy(int value) {
+        count -= value;
+    }
+
+    public void multiplyBy(int value) {
+        count *= value;
+    }
 }
