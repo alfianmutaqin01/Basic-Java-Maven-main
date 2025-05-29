5d01a9616221103552c99f958ddc4bce25a18e26
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-05-29 15:27:47 +0700

    Enhance Counter class functionality with increaseBy, decreaseBy, and multiplyBy methods; update Driver class to demonstrate new features.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,21 +2,19 @@ import src.main.java.Counter;
 
 public class Driver {
 
-	public static void main(String[] args) {
-		
-		Counter counter = new Counter();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.increaseBy();
-		
-		System.out.println("Current count:" + counter.getCount());
-		
-		counter.decreaseBy();
-		
-		System.out.println("Current count:" + counter.getCount());
-		counter.multiplyBy(3);
-		
-	}
+    public static void main(String[] args) {
+        
+        Counter counter = new Counter();
+        
+        System.out.println("Current count: " + counter.getCount());
 
+        counter.increaseBy(5); 
+        System.out.println("After increase: " + counter.getCount());
+
+        counter.decreaseBy(2); 
+        System.out.println("After decrease: " + counter.getCount());
+
+        counter.multiplyBy(3); 
+        System.out.println("After multiplication: " + counter.getCount());
+    }
 }
