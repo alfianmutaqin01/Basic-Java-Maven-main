f8b530420660c9e81059311313934a9bf50aca74
Alfian Mutakim <alfianmutaqien@gmail.com>
2025-05-29 15:36:50 +0700

    .

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,19 +2,21 @@ import src.main.java.Counter;
 
 public class Driver {
 
-    public static void main(String[] args) {
-        
-        Counter counter = new Counter();
-        
-        System.out.println("Current count: " + counter.getCount());
+	public static void main(String[] args) {
+		
+		Counter counter = new Counter();
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.increaseBy(5);
+		
+		System.out.println("Current count:" + counter.getCount());
+		
+		counter.decreaseBy(2);
+		
+		System.out.println("Current count:" + counter.getCount());
+		counter.multiplyBy(3);
+		
+	}
 
-        counter.increaseBy(5); 
-        System.out.println("After increase: " + counter.getCount());
-
-        counter.decreaseBy(2); 
-        System.out.println("After decrease: " + counter.getCount());
-
-        counter.multiplyBy(3); 
-        System.out.println("After multiplication: " + counter.getCount());
-    }
 }
